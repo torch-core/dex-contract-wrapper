8985f770164d170c8787b7473e2dbaecaffd14d5
ipromise2324 <168allen55@gmail.com>
2025-01-17 01:59:25 +0800

    feat: implement simulation methods for deposit, swap, and withdraw in Pool class
    - Added simulateDeposit, getSimulateSwap, and getSimulateWithdraw methods to the Pool class for enhanced liquidity management.
    - Introduced new types for simulation parameters and results, improving type safety and clarity.
    - Updated logic to handle asset amounts and virtual prices during simulations, ensuring accurate calculations.
    
    These enhancements provide a robust framework for simulating pool operations, paving the way for improved user experience and functionality.

--- src/pool/index.ts
+++ src/pool/index.ts
@@ -2,6 +2,14 @@ import { Address, ContractProvider, Contract } from '@ton/core';
 import { beginCell } from '@ton/core';
 import { Allocation, Asset, parseAssetsFromNestedCell, storeCoinsNested } from '@torch-finance/core';
 import { parsePool, PoolData } from './storage';
+import {
+  SimulateDepositParams,
+  SimulateSwapParams,
+  SimulateWithdrawParams,
+  SimulateWithdrawResult,
+  SimulatorDepositResult,
+  SimulatorSwapResult,
+} from './simulate';
 
 export class Pool implements Contract {
   constructor(readonly address: Address) {}
@@ -51,5 +59,104 @@ export class Pool implements Contract {
     const virtualPrice = vp.stack.readBigNumber();
 
     return virtualPrice;
+  }
+
+  // Simulate
+  async simulateDeposit(provider: ContractProvider, params: SimulateDepositParams): Promise<SimulatorDepositResult> {
+    const res = await provider.get('simulate_deposit', [
+      {
+        type: 'cell',
+        cell: storeCoinsNested(params.depositAmounts.map((amount) => amount.amount)),
+      },
+      params.rates
+        ? {
+            type: 'cell',
+            cell: storeCoinsNested(params.rates.map((rate) => rate.amount)),
+          }
+        : {
+            type: 'null',
+          },
+    ]);
+
+    const lpTokenOut = res.stack.readBigNumber();
+    const virtualPriceBefore = res.stack.readBigNumber();
+    const virtualPriceAfter = res.stack.readBigNumber();
+    const lpTotalSupply = res.stack.readBigNumber();
+
+    return { lpTokenOut, virtualPriceBefore, virtualPriceAfter, lpTotalSupply };
+  }
+
+  async getSimulateSwap(provider: ContractProvider, params: SimulateSwapParams): Promise<SimulatorSwapResult> {
+    const simulateSwap = await provider.get('get_simulate_swap', [
+      {
+        type: 'cell',
+        cell: params.assetIn.toCell(),
+      },
+      {
+        type: 'cell',
+        cell: params.assetOut.toCell(),
+      },
+      {
+        type: 'int',
+        value: params.amount,
+      },
+      params.rates
+        ? {
+            type: 'cell',
+            cell: storeCoinsNested(params.rates.map((rate) => rate.amount)),
+          }
+        : {
+            type: 'null',
+          },
+    ]);
+
+    const amountOut = simulateSwap.stack.readBigNumber();
+    const virtualPriceBefore = simulateSwap.stack.readBigNumber();
+    const virtualPriceAfter = simulateSwap.stack.readBigNumber();
+    return {
+      amountOut,
+      virtualPriceBefore,
+      virtualPriceAfter,
+    };
+  }
+
+  async getSimulateWithdraw(
+    provider: ContractProvider,
+    params: SimulateWithdrawParams,
+  ): Promise<SimulateWithdrawResult> {
+    const simulateRemoveLiquidity = await provider.get('get_simulate_withdraw', [
+      {
+        type: 'int',
+        value: params.lpAmount,
+      },
+      {
+        type: 'cell',
+        cell: beginCell()
+          .storeMaybeRef(params.assetOut?.toCell() ?? null)
+          .endCell(),
+      },
+      params.rates
+        ? {
+            type: 'cell',
+            cell: storeCoinsNested(params.rates.map((rate) => rate.amount)),
+          }
+        : {
+            type: 'null',
+          },
+    ]);
+    const amountOutsTuple = simulateRemoveLiquidity.stack.readTuple();
+
+    const amountOuts = [];
+    while (amountOutsTuple.remaining > 0) {
+      amountOuts.push(amountOutsTuple.readBigNumber());
+    }
+    const virtualPriceBefore = simulateRemoveLiquidity.stack.readBigNumber();
+    const virtualPriceAfter = simulateRemoveLiquidity.stack.readBigNumber();
+
+    return {
+      amountOuts,
+      virtualPriceBefore,
+      virtualPriceAfter,
+    };
   }
 }
