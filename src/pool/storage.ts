cb0807960d7e9a53369ff20b2f5dab220764c15f
alan890104 <alan890104@gmail.com>
2025-01-20 01:50:40 +0800

    feat: implement LpAccount and Pool contracts with simulation capabilities
    - Introduced LpAccount and Pool classes to manage liquidity pool accounts and operations.
    - Added methods for retrieving account data, simulating deposits, swaps, and withdrawals in the Pool class.
    - Enhanced data parsing and structure for pool-related information, including asset management and virtual pricing.
    - Refactored index files to streamline exports and improve module organization.
    
    These changes lay the groundwork for advanced liquidity management features and improve the overall architecture of the contract system.

--- src/pool/storage.ts
+++ src/pool/storage.ts
@@ -1,43 +1,7 @@
-import { Address, Cell, Dictionary, Slice } from '@ton/core';
+import { Dictionary, Slice } from '@ton/core';
 import { ContractType, PoolStatus, PoolType, Size } from '../common';
-import { Allocation, Asset, parseAssetsFromNestedCell, parseCoinsFromNestedCell } from '@torch-finance/core';
-
-export type ReserveData = {
-  reserves: Allocation[];
-  adminFees: Allocation[];
-};
-
-export type BasicData = {
-  feeNumerator: bigint;
-  adminFeeNumerator: bigint;
-  initA: number;
-  futureA: number;
-  initATime: number;
-  futureATime: number;
-  lpTotalSupply: bigint;
-  lpWalletCode: Cell;
-  decimals: Allocation[];
-  plugins: Dictionary<bigint, Cell>;
-};
-
-export type ProofData = {
-  baseCode: Cell;
-  factory: Address;
-};
-
-export type PoolData = {
-  contractType: ContractType;
-  poolType: PoolType;
-  admin: Address;
-  signerKey: Buffer; // signer key for pool
-  status: PoolStatus; // current status of pool (not exists, active, is stop)
-  useRate: boolean; // use external reference redemption rate for yield bearing stable pool
-  baseLpIndex: number; // LP Token index in the pool, only for meta pool. In base pool, it's always 0.
-  assets: Asset[]; // assets in the pool, already sorted
-  reserveData: ReserveData;
-  basicData: BasicData;
-  proofData: ProofData;
-};
+import { Allocation, parseAssetsFromNestedCell, parseCoinsFromNestedCell } from '@torch-finance/core';
+import { PoolData } from './types';
 
 export function parsePool(sc: Slice): PoolData {
   const contractType = sc.loadUint(Size.ContractType) as ContractType;
@@ -54,7 +18,7 @@ export function parsePool(sc: Slice): PoolData {
   const assets = parseAssetsFromNestedCell(sc.loadRef());
   const reserveCell = sc.loadRef();
   const reserveSc = reserveCell.beginParse();
-  const reserveData: ReserveData = {
+  const reserveData = {
     reserves: parseCoinsFromNestedCell(reserveSc.loadRef()).map((item, index) => {
       return new Allocation({ asset: assets[index], value: item });
     }),
@@ -64,7 +28,7 @@ export function parsePool(sc: Slice): PoolData {
   };
   const basicInfoCell = sc.loadRef();
   const basicInfoSc = basicInfoCell.beginParse();
-  const basicData: BasicData = {
+  const basicData = {
     feeNumerator: basicInfoSc.loadCoins(),
     adminFeeNumerator: basicInfoSc.loadCoins(),
     initA: basicInfoSc.loadUint(Size.AmplificationFactor),
