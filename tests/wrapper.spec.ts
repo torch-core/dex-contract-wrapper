ef1cf6e523e03b549252d14cd9b0d954a5c1318f
ipromise2324 <168allen55@gmail.com>
2025-01-26 17:50:04 +0800

    test: enhance wrapper test suite with USD pool and virtual price test cases
    - Added test cases for USD pools in the wrapper test suite
    - Implemented tests for getVirtualPrice() method with and without rates
    - Added simulateDeposit(), simulateSwap(), and simulateWithdraw() test cases for USD pools
    - Refactored constant names to use UPPERCASE for better readability
    - Introduced new asset constants for USD-related tokens

--- tests/wrapper.spec.ts
+++ tests/wrapper.spec.ts
@@ -9,37 +9,48 @@ import { Allocation, Asset } from '@torch-finance/core';
 describe('Wrapper Testcases', () => {
   const endpoint = 'https://testnet-v4.tonhubapi.com';
   const client = new TonClient4({ endpoint });
-  const factoryAddress = Address.parse('EQBO9Xw9w0hJQx4kw3RSKu2LROZbtKg4icITKYp5enCQVGCu');
-  const triTONPoolAddress = Address.parse('EQCEao02tugbZjudFRMfyu2s_nVZli7F_rgxC1OjdvXpsBsw');
-  const quaTONPoolAddress = Address.parse('EQA4rUktNrzOmgZ4OzsOX5Q-C1KelFPCtH8ln2YaHgyAO4kc');
-  const tonAsset = Asset.ton();
-  const tsTONAsset = Asset.jetton('EQA5rOnkPx8xTWvSjKAqEkdLOIM0-IyT_u-5IEQ5R2y9m-36');
-  const stTONAsset = Asset.jetton('EQBbKadthJqQfnEsijYFvi25AKGDhS3CTVAf8oGZYwGk8G8W');
-  const triTONAsset = Asset.jetton(triTONPoolAddress);
-  const hTONAsset = Asset.jetton('EQDInlQkBcha9-KPGDR-eWi5VGhYPXO5s04amtzZ07s0Kzuu');
+  const FACTORY_ADDRESS = Address.parse('EQBO9Xw9w0hJQx4kw3RSKu2LROZbtKg4icITKYp5enCQVGCu');
+  const TRI_TON_POOL_ADDRESS = Address.parse('EQCEao02tugbZjudFRMfyu2s_nVZli7F_rgxC1OjdvXpsBsw');
+  const QUA_TON_POOL_ADDRESS = Address.parse('EQA4rUktNrzOmgZ4OzsOX5Q-C1KelFPCtH8ln2YaHgyAO4kc');
+  const TON_ASSET = Asset.ton();
+  const TS_TON_ASSET = Asset.jetton('EQA5rOnkPx8xTWvSjKAqEkdLOIM0-IyT_u-5IEQ5R2y9m-36');
+  const ST_TON_ASSET = Asset.jetton('EQBbKadthJqQfnEsijYFvi25AKGDhS3CTVAf8oGZYwGk8G8W');
+  const TRI_TON_ASSET = Asset.jetton(TRI_TON_POOL_ADDRESS);
+  const USDT_ASSET = Asset.jetton(Address.parse('EQBflht80hwbivqv3Hnlhigqfe4RdY4Kb-LSOVldvGBsAgOQ'));
+  const USDC_ASSET = Asset.jetton(Address.parse('EQARxQlZfQUxhTcCRg4QraCtxmvw1GoGOeEanbcc55wLZg3E'));
+  const CRV_USD_ASSET = Asset.jetton(Address.parse('EQC76HKO16zcESvqLzDXpV98uRNiPDl_TO-g6794VMDGbbNZ'));
+
+  const TRI_USD_POOL_ADDRESS = Address.parse('EQCP0zt6jVBBQrfuVQv2mkGxTx644BY0givW2BskBkJ7oQoN');
+  const QUA_USD_POOL_ADDRESS = Address.parse('EQDNrykzaG7kEzmqa0H7nRRudU8EtzDSzYVQ8QEPslOgwDG8');
+
+  const H_TONAsset = Asset.jetton('EQDInlQkBcha9-KPGDR-eWi5VGhYPXO5s04amtzZ07s0Kzuu');
   const jettonVaultAddress = Address.parse('EQDruot_WmgJfqy3sz6VwjM5h48eioAblY_tL_j_ZuNcj6nU');
   const tonVaultAddress = Address.parse('EQDB8wYs5U_alBNFPpR9UpI8wQNGhAEJmgrR8kOjSX50gOlH');
   const senderAddress = Address.parse('0QBtvbUwvUMHWiYt85cqAjtMSTOoDCufuBEhh7m6czZTn0wF');
 
   const triTONPoolRate: Allocation[] = Allocation.createAllocations([
-    { asset: tonAsset, value: 10n ** 27n },
-    { asset: tsTONAsset, value: 10n ** 27n },
-    { asset: stTONAsset, value: 10n ** 27n },
+    { asset: TON_ASSET, value: 10n ** 27n },
+    { asset: TS_TON_ASSET, value: 10n ** 27n },
+    { asset: ST_TON_ASSET, value: 10n ** 27n },
   ]).sort((a, b) => a.asset.compare(b.asset));
 
   const quaTONPoolRate: Allocation[] = Allocation.createAllocations([
-    { asset: hTONAsset, value: 10n ** 27n },
-    { asset: triTONAsset, value: 10n ** 18n },
+    { asset: H_TONAsset, value: 10n ** 27n },
+    { asset: TRI_TON_ASSET, value: 10n ** 18n },
   ]).sort((a, b) => a.asset.compare(b.asset));
 
   let factory: OpenedContract<Factory>;
   let triTONPool: OpenedContract<Pool>;
   let quaTONPool: OpenedContract<Pool>;
+  let triUSDPOOL: OpenedContract<Pool>;
+  let quaUSDPOOL: OpenedContract<Pool>;
 
   beforeEach(async () => {
-    factory = client.open(Factory.createFromAddress(factoryAddress));
-    triTONPool = client.open(Pool.createFromAddress(triTONPoolAddress));
-    quaTONPool = client.open(Pool.createFromAddress(quaTONPoolAddress));
+    factory = client.open(Factory.createFromAddress(FACTORY_ADDRESS));
+    triTONPool = client.open(Pool.createFromAddress(TRI_TON_POOL_ADDRESS));
+    quaTONPool = client.open(Pool.createFromAddress(QUA_TON_POOL_ADDRESS));
+    triUSDPOOL = client.open(Pool.createFromAddress(TRI_USD_POOL_ADDRESS));
+    quaUSDPOOL = client.open(Pool.createFromAddress(QUA_USD_POOL_ADDRESS));
   });
 
   describe('Factory get-methods testcases', () => {
@@ -56,10 +67,10 @@ describe('Wrapper Testcases', () => {
     it('should getDepositPayload() successfully', async () => {
       const depositParams: DepositPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         poolAllocations: Allocation.createAllocations([
-          { asset: tonAsset, value: 1000000000000000000n },
-          { asset: tsTONAsset, value: 1000000000000000000n },
+          { asset: TON_ASSET, value: 1000000000000000000n },
+          { asset: TS_TON_ASSET, value: 1000000000000000000n },
         ]),
       };
       const payload = await factory.getDepositPayload(senderAddress, depositParams);
@@ -69,15 +80,15 @@ describe('Wrapper Testcases', () => {
     it('should getDepositPayload() with deposit next successfully', async () => {
       const depositParams: DepositPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         poolAllocations: Allocation.createAllocations([
-          { asset: tonAsset, value: 1000000000000000000n },
-          { asset: tsTONAsset, value: 1000000000000000000n },
+          { asset: TON_ASSET, value: 1000000000000000000n },
+          { asset: TS_TON_ASSET, value: 1000000000000000000n },
         ]),
         next: {
           type: 'Deposit',
-          nextPoolAddress: quaTONPoolAddress,
-          metaAllocation: new Allocation({ asset: tonAsset, value: 1000000000000000000n }),
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
+          metaAllocation: new Allocation({ asset: TON_ASSET, value: 1000000000000000000n }),
         },
       };
       const payload = await factory.getDepositPayload(senderAddress, depositParams);
@@ -87,15 +98,15 @@ describe('Wrapper Testcases', () => {
     it('should getDepositPayload() with swap next successfully', async () => {
       const depositParams: DepositPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         poolAllocations: Allocation.createAllocations([
-          { asset: tonAsset, value: 1000000000000000000n },
-          { asset: tsTONAsset, value: 1000000000000000000n },
+          { asset: TON_ASSET, value: 1000000000000000000n },
+          { asset: TS_TON_ASSET, value: 1000000000000000000n },
         ]),
         next: {
           type: 'Swap',
-          nextPoolAddress: quaTONPoolAddress,
-          assetOut: tsTONAsset,
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
+          assetOut: TS_TON_ASSET,
         },
       };
       const payload = await factory.getDepositPayload(senderAddress, depositParams);
@@ -105,9 +116,9 @@ describe('Wrapper Testcases', () => {
     it('should call getSwapPayload() successfully', async () => {
       const swapParams: SwapPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
-        assetIn: tonAsset,
-        assetOut: tsTONAsset,
+        poolAddress: TRI_TON_POOL_ADDRESS,
+        assetIn: TON_ASSET,
+        assetOut: TS_TON_ASSET,
         amountIn: 1000000000000000000n,
       };
       const payload = await factory.getSwapPayload(senderAddress, swapParams);
@@ -117,16 +128,16 @@ describe('Wrapper Testcases', () => {
     it('should call getSwapPayload() with withdraw single mode next successfully', async () => {
       const swapParams: SwapPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
-        assetIn: tonAsset,
-        assetOut: tsTONAsset,
+        poolAddress: TRI_TON_POOL_ADDRESS,
+        assetIn: TON_ASSET,
+        assetOut: TS_TON_ASSET,
         amountIn: 1000000000000000000n,
         next: {
           type: 'Withdraw',
-          nextPoolAddress: quaTONPoolAddress,
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
           config: {
             mode: 'Single',
-            assetOut: tonAsset,
+            assetOut: TON_ASSET,
           },
         },
       };
@@ -137,13 +148,13 @@ describe('Wrapper Testcases', () => {
     it('should call getSwapPayload() with withdraw balanced mode next successfully', async () => {
       const swapParams: SwapPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
-        assetIn: tonAsset,
-        assetOut: tsTONAsset,
+        poolAddress: TRI_TON_POOL_ADDRESS,
+        assetIn: TON_ASSET,
+        assetOut: TS_TON_ASSET,
         amountIn: 1000000000000000000n,
         next: {
           type: 'Withdraw',
-          nextPoolAddress: quaTONPoolAddress,
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
           config: {
             mode: 'Balanced',
           },
@@ -156,7 +167,7 @@ describe('Wrapper Testcases', () => {
     it('should call getWithdrawPayload() with balanced mode successfully', async () => {
       const withdrawParams: WithdrawPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         burnLpAmount: 1000000000000000000n,
         config: {
           mode: 'Balanced',
@@ -169,14 +180,14 @@ describe('Wrapper Testcases', () => {
     it('should call getWithdrawPayload() with balanced mode and next balanced mode successfully', async () => {
       const withdrawParams: WithdrawPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         burnLpAmount: 1000000000000000000n,
         config: {
           mode: 'Balanced',
         },
         next: {
           type: 'Withdraw',
-          nextPoolAddress: quaTONPoolAddress,
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
           config: {
             mode: 'Balanced',
           },
@@ -189,17 +200,17 @@ describe('Wrapper Testcases', () => {
     it('should call getWithdrawPayload() with balanced mode and next single mode successfully', async () => {
       const withdrawParams: WithdrawPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         burnLpAmount: 1000000000000000000n,
         config: {
           mode: 'Balanced',
         },
         next: {
           type: 'Withdraw',
-          nextPoolAddress: quaTONPoolAddress,
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
           config: {
             mode: 'Single',
-            assetOut: tonAsset,
+            assetOut: TON_ASSET,
           },
         },
       };
@@ -210,11 +221,11 @@ describe('Wrapper Testcases', () => {
     it('should call getWithdrawPayload() with single mode successfully', async () => {
       const withdrawParams: WithdrawPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         burnLpAmount: 1000000000000000000n,
         config: {
           mode: 'Single',
-          assetOut: tonAsset,
+          assetOut: TON_ASSET,
         },
       };
       const payload = await factory.getWithdrawPayload(senderAddress, withdrawParams);
@@ -224,15 +235,15 @@ describe('Wrapper Testcases', () => {
     it('should call getWithdrawPayload() with single mode and next balanced mode successfully', async () => {
       const withdrawParams: WithdrawPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         burnLpAmount: 1000000000000000000n,
         config: {
           mode: 'Single',
-          assetOut: tonAsset,
+          assetOut: TON_ASSET,
         },
         next: {
           type: 'Withdraw',
-          nextPoolAddress: quaTONPoolAddress,
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
           config: {
             mode: 'Balanced',
           },
@@ -245,18 +256,18 @@ describe('Wrapper Testcases', () => {
     it('should call getWithdrawPayload() with single mode and next single mode successfully', async () => {
       const withdrawParams: WithdrawPayload = {
         queryId: 1n,
-        poolAddress: triTONPoolAddress,
+        poolAddress: TRI_TON_POOL_ADDRESS,
         burnLpAmount: 1000000000000000000n,
         config: {
           mode: 'Single',
-          assetOut: tonAsset,
+          assetOut: TON_ASSET,
         },
         next: {
           type: 'Withdraw',
-          nextPoolAddress: quaTONPoolAddress,
+          nextPoolAddress: QUA_TON_POOL_ADDRESS,
           config: {
             mode: 'Single',
-            assetOut: tonAsset,
+            assetOut: TON_ASSET,
           },
         },
       };
@@ -288,7 +299,7 @@ describe('Wrapper Testcases', () => {
       expect(walletAddress2).toBeDefined();
     });
 
-    it('should call getVirtualPrice() successfully', async () => {
+    it('should call getVirtualPrice() with rates successfully', async () => {
       const virtualPrice = await triTONPool.getVirtualPrice(triTONPoolRate);
 
       const virtualPrice2 = await quaTONPool.getVirtualPrice(quaTONPoolRate);
@@ -296,12 +307,28 @@ describe('Wrapper Testcases', () => {
       expect(virtualPrice2).toBeDefined();
     });
 
-    it('should call simulateDeposit() successfully', async () => {
+    it('should call getVirtualPrice() without rates successfully', async () => {
+      const virtualPrice = await triUSDPOOL.getVirtualPrice();
+
+      const SCRV_USD_ASSET = Asset.jetton(Address.parse('EQBN8qMhmCS2yj9a7KqRJTGPv8AZmfsBnRrw3ClODwpyus8v'));
+      const TRI_USD_LP_ASSET = Asset.jetton(TRI_USD_POOL_ADDRESS);
+
+      const quaUSDPOOLRate = Allocation.createAllocations([
+        { asset: SCRV_USD_ASSET, value: 10n ** 18n },
+        { asset: TRI_USD_LP_ASSET, value: 10n ** 18n },
+      ]).sort((a, b) => a.asset.compare(b.asset));
+
+      const virtualPrice2 = await quaUSDPOOL.getVirtualPrice(quaUSDPOOLRate);
+      expect(virtualPrice).toBeDefined();
+      expect(virtualPrice2).toBeDefined();
+    });
+
+    it('should call simulateDeposit() with rates successfully', async () => {
       const simulateDepositResult = await triTONPool.getSimulateDeposit({
         depositAmounts: Allocation.createAllocations([
-          { asset: tonAsset, value: 1000000000000000000n },
-          { asset: tsTONAsset, value: 1000000000000000000n },
-          { asset: stTONAsset, value: 1000000000000000000n },
+          { asset: TON_ASSET, value: 1000000000000000000n },
+          { asset: TS_TON_ASSET, value: 1000000000000000000n },
+          { asset: ST_TON_ASSET, value: 1000000000000000000n },
         ]),
         rates: triTONPoolRate,
       });
@@ -311,11 +338,27 @@ describe('Wrapper Testcases', () => {
       expect(simulateDepositResult.virtualPriceAfter).toBeDefined();
       expect(simulateDepositResult.lpTotalSupply).toBeDefined();
     });
-    it('should invoke simulateSwap() with ExactIn successfully', async () => {
+
+    it('should call simulateDeposit() without rates successfully', async () => {
+      const simulateDepositResult = await triUSDPOOL.getSimulateDeposit({
+        depositAmounts: Allocation.createAllocations([
+          { asset: USDT_ASSET, value: 100000n },
+          { asset: USDC_ASSET, value: 100000n },
+          { asset: CRV_USD_ASSET, value: 10n ** 17n },
+        ]),
+      });
+      expect(simulateDepositResult).toBeDefined();
+      expect(simulateDepositResult.lpTokenOut).toBeDefined();
+      expect(simulateDepositResult.virtualPriceBefore).toBeDefined();
+      expect(simulateDepositResult.virtualPriceAfter).toBeDefined();
+      expect(simulateDepositResult.lpTotalSupply).toBeDefined();
+    });
+
+    it('should call simulateSwap() with rates and ExactIn successfully', async () => {
       const simulateSwapResult = await triTONPool.getSimulateSwap({
         mode: 'ExactIn',
-        assetIn: tonAsset,
-        assetOut: tsTONAsset,
+        assetIn: TON_ASSET,
+        assetOut: TS_TON_ASSET,
         amountIn: 1000000000000000000n,
         rates: triTONPoolRate,
       });
@@ -329,11 +372,27 @@ describe('Wrapper Testcases', () => {
       expect(simulateSwapResult.virtualPriceAfter).toBeDefined();
     });
 
-    it('should call simulateSwap() with ExactOut successfully', async () => {
+    it('should call simulateSwap() without rates but with ExactIn successfully', async () => {
+      const simulateSwapResult = await triUSDPOOL.getSimulateSwap({
+        mode: 'ExactIn',
+        assetIn: USDT_ASSET,
+        assetOut: USDC_ASSET,
+        amountIn: 100000n,
+      });
+      if (simulateSwapResult.mode !== 'ExactIn') {
+        throw new Error('simulateSwapResult.mode is not ExactIn');
+      }
+      expect(simulateSwapResult).toBeDefined();
+      expect(simulateSwapResult.amountOut).toBeDefined();
+      expect(simulateSwapResult.virtualPriceBefore).toBeDefined();
+      expect(simulateSwapResult.virtualPriceAfter).toBeDefined();
+    });
+
+    it('should call simulateSwap() with rates and ExactOut successfully', async () => {
       const simulateSwapResult = await triTONPool.getSimulateSwap({
         mode: 'ExactOut',
-        assetIn: tonAsset,
-        assetOut: tsTONAsset,
+        assetIn: TON_ASSET,
+        assetOut: TS_TON_ASSET,
         amountOut: toNano(1),
         rates: triTONPoolRate,
       });
@@ -344,6 +403,45 @@ describe('Wrapper Testcases', () => {
       expect(simulateSwapResult.amountIn).toBeDefined();
       expect(simulateSwapResult.virtualPriceBefore).toBeDefined();
       expect(simulateSwapResult.virtualPriceAfter).toBeDefined();
+    });
+
+    it('should call simulateSwap() without rates but with ExactOut successfully', async () => {
+      const simulateSwapResult = await triUSDPOOL.getSimulateSwap({
+        mode: 'ExactOut',
+        assetIn: USDT_ASSET,
+        assetOut: USDC_ASSET,
+        amountOut: 1000n,
+      });
+      if (simulateSwapResult.mode != 'ExactOut') {
+        throw new Error('simulateSwapResult.mode is not ExactOut');
+      }
+
+      expect(simulateSwapResult).toBeDefined();
+      expect(simulateSwapResult.amountIn).toBeDefined();
+      expect(simulateSwapResult.virtualPriceBefore).toBeDefined();
+      expect(simulateSwapResult.virtualPriceAfter).toBeDefined();
+    });
+
+    it('should call simulateWithdraw() with rates successfully', async () => {
+      const simulateWithdrawResult = await triTONPool.getSimulateWithdraw({
+        lpAmount: 1000000000000000000n,
+        rates: triTONPoolRate,
+      });
+
+      expect(simulateWithdrawResult).toBeDefined();
+      expect(simulateWithdrawResult.amountOuts).toBeDefined();
+      expect(simulateWithdrawResult.virtualPriceBefore).toBeDefined();
+      expect(simulateWithdrawResult.virtualPriceAfter).toBeDefined();
+    });
+
+    it('should call simulateWithdraw() without rates successfully', async () => {
+      const simulateWithdrawResult = await triUSDPOOL.getSimulateWithdraw({
+        lpAmount: 1000000000000000000n,
+      });
+      expect(simulateWithdrawResult).toBeDefined();
+      expect(simulateWithdrawResult.amountOuts).toBeDefined();
+      expect(simulateWithdrawResult.virtualPriceBefore).toBeDefined();
+      expect(simulateWithdrawResult.virtualPriceAfter).toBeDefined();
     });
   });
 
