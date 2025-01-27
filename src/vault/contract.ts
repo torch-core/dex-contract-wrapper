1e87d7f6b96a53c8c80b9bea96182d72df465843
ipromise2324 <168allen55@gmail.com>
2025-01-27 23:54:58 +0800

    feat: add contract type and asset type to vault data structures

--- src/vault/contract.ts
+++ src/vault/contract.ts
@@ -1,6 +1,7 @@
 import { Address, Contract, ContractProvider } from '@ton/core';
 import { AssetType } from '@torch-finance/core';
 import { TonVaultData, JettonVaultData } from './storage';
+import { ContractType, Size } from '../common';
 
 export class Vault implements Contract {
   constructor(readonly address: Address) {}
@@ -12,16 +13,18 @@ export class Vault implements Contract {
   async getVaultData(provider: ContractProvider): Promise<TonVaultData | JettonVaultData> {
     const res = await provider.get('get_vault_data', []);
     const sc = res.stack.readCell().beginParse();
-    const assetType = sc.loadUint(4) as AssetType;
+    const contractType = sc.loadUint(Size.ContractType) as ContractType;
+    const assetType = sc.loadUint(Size.AssetType) as AssetType;
     const admin = sc.loadAddress(); // admin address
 
     switch (assetType) {
       case AssetType.TON:
-        return { assetType, admin } as TonVaultData;
+        return { contractType, assetType, admin } as TonVaultData;
       case AssetType.JETTON: {
         const jettonMaster = sc.loadAddress();
         const jettonWallet = sc.loadAddress();
         return {
+          contractType,
           assetType,
           admin,
           jettonMaster,
--- src/vault/storage.ts
+++ src/vault/storage.ts
@@ -1,12 +1,17 @@
 import { Address } from '@ton/core';
 import { AssetType } from '@torch-finance/core';
+import { ContractType } from '../common';
 
 export type TonVaultData = {
+  contractType: ContractType;
   assetType: AssetType;
   admin: Address;
 };
 
 export type JettonVaultData = TonVaultData & {
+  contractType: ContractType;
+  assetType: AssetType;
+  admin: Address;
   jettonMaster: Address;
   jettonWallet: Address;
 };
